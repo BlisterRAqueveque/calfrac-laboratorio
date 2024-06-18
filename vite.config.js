502d21786f8e561384808289cf9f3d10a876fae2
BlisterRAqueveque <141036948+BlisterRAqueveque@users.noreply.github.com>
2024-06-18 12:37:09 -0300

    stable version

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-	"resources/css/app.css",
+  plugins: [
+    laravel({
+      input: [
+        "resources/css/app.css",
         "resources/css/bootstrap.min.css",
         "resources/css/sb-admin-2.css",
         "resources/css/solicitud.css",
         "resources/js/app.js",
         "resources/js/bootstrap.bundle.min.js",
         "resources/js/bootstrap.js",
-            ],
-            refresh: true,
-        }),
-    ],
+      ],
+      refresh: true,
+    }),
+  ],
 });
