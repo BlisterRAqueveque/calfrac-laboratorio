3345fe708449011c8d8ed95d20a0d9c135b28303
BlisterRAqueveque <141036948+BlisterRAqueveque@users.noreply.github.com>
2024-06-18 12:16:26 -0300

    Stable version

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
+	"resources/css/app.css",
+        "resources/css/bootstrap.min.css",
+        "resources/css/sb-admin-2.css",
+        "resources/css/solicitud.css",
+        "resources/js/app.js",
+        "resources/js/bootstrap.bundle.min.js",
+        "resources/js/bootstrap.js",
             ],
             refresh: true,
         }),
