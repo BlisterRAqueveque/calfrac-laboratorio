3b65a101f251551f75150ce9d6efb817dc00152f
RooCarvajal <rocio.carvajal@blistertechnologies.com>
2024-08-22 15:44:21 -0300

    ready img reologia

--- public/js/ensayo/create/componentReologia.js
+++ public/js/ensayo/create/componentReologia.js
@@ -14,6 +14,14 @@ let accordionItem,
   tbody,
   tr,
   th,
+  div_img,
+  section,
+  div_relative,
+  contain_img,
+  img,
+  div_txt,
+  h6,
+  div_flex,
   td;
 const accordionReologia = document.getElementById("accordionReologia");
 
@@ -247,6 +255,33 @@ const componentReologia = (data = "") => {
 
   mount(accordionItem, table)
 
+  div_img = el('div.mb-2 mt-2 text-center')
+  h5 = el('h5.mb-1', 'Adjunto')
+  section = el('section')
+  div_relative = el('div.relative md:w-1/2 xl:w-1/3 mx-auto flex items-center p-2 border rounded-md border-gray-200 hover:bg-gray-50')
+  contain_img = el('div.w-12 h-12 rounded-full bg-gray-100 flex-shrink-0')
+  img = el('img.w-12 h-12 object-cover rounded-full cursor-pointer')
+  img.setAttribute('src', _setUrl('uploads/ensayos', data.img_up))
+  img.setAttribute('onclick', `openModal('myModalReologiaUp', 'modalImgReologiaUp', '${_setUrl('uploads/ensayos', data.img_up)}')`)
+  mount(contain_img, img)
+  mount(div_relative, contain_img)
+
+  div_txt = el('div.text-left w-full ms-2')
+  h6 = el('text-md', 'Archivo adjunto')
+  div_flex = el('div.flex justify-between items-center text-gray-500 text-sm', el('p.mb-0', data.img_up))
+
+  mount(div_txt, h6)
+  mount(div_txt, div_flex)
+
+  mount(div_relative, div_txt)
+  mount(section, div_relative)
+  
+  mount(div_img, h5)
+  mount(div_img, section)
+
+  // mount(registro_uca, divGrid)
+  mount(accordionItem, div_img)
+
   h5_4 = el("h5.mb-1 text-center ", "Reología (down reads)");
   br = el("br");
 
@@ -326,32 +361,33 @@ const componentReologia = (data = "") => {
 
   mount(accordionItem, table)
 
-  // let div_img = el('div.mb-2 mt-2 text-center')
-  // let h5 = el('h5.mb-1', 'Adjunto')
-  // let section = el('section')
-  // let div_relative = el('div.relative md:w-1/2 xl:w-1/3 mx-auto flex items-center p-2 border rounded-md border-gray-200 hover:bg-gray-50')
-  // let contain_img = el('div.w-12 h-12 rounded-full bg-gray-100 flex-shrink-0')
-  // let img = el('img.w-12 h-12 object-cover rounded-full cursor-pointer')
-  // img.setAttribute('src', _setUrl('uploads/ensayos', data.img))
-  // img.setAttribute('onclick', `openModal('myModalUca', 'modalImgUca', '${_setUrl('uploads/ensayos', data.img)}')`)
-  // mount(contain_img, img)
-  // mount(div_relative, contain_img)
+  div_img = el('div.mb-2 mt-2 text-center')
+  h5 = el('h5.mb-1', 'Adjunto')
+  section = el('section')
+  div_relative = el('div.relative md:w-1/2 xl:w-1/3 mx-auto flex items-center p-2 border rounded-md border-gray-200 hover:bg-gray-50')
+  contain_img = el('div.w-12 h-12 rounded-full bg-gray-100 flex-shrink-0')
+  img = el('img.w-12 h-12 object-cover rounded-full cursor-pointer')
+  img.setAttribute('src', _setUrl('uploads/ensayos', data.img_down))
+  img.setAttribute('onclick', `openModal('modalImgReologiaDown', 'modalImgReologiaDown', '${_setUrl('uploads/ensayos', data.img_down)}')`)
+  mount(contain_img, img)
+  mount(div_relative, contain_img)
 
-  // let div_txt = el('div.text-left w-full ms-2')
-  // let h6 = el('text-md', 'Archivo adjunto')
-  // let div_flex = el('div.flex justify-between items-center text-gray-500 text-sm', el('p.mb-0', data.img))
+  div_txt = el('div.text-left w-full ms-2')
+  h6 = el('text-md', 'Archivo adjunto')
+  div_flex = el('div.flex justify-between items-center text-gray-500 text-sm', el('p.mb-0', data.img_down))
 
-  // mount(div_txt, h6)
-  // mount(div_txt, div_flex)
+  mount(div_txt, h6)
+  mount(div_txt, div_flex)
 
-  // mount(div_relative, div_txt)
-  // mount(section, div_relative)
+  mount(div_relative, div_txt)
+  mount(section, div_relative)
   
-  // mount(div_img, h5)
-  // mount(div_img, section)
+  mount(div_img, h5)
+  mount(div_img, section)
 
   // mount(registro_uca, divGrid)
-  // mount(registro_uca, div_img)
+  mount(accordionItem, div_img)
+
 
 
 };
