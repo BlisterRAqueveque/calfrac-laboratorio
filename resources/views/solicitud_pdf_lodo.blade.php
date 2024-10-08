8f19c16671b1288e9e9e89521b1e37ac3f858a2e
RooCarvajal <rocio.carvajal@blistertechnologies.com>
2024-10-08 11:34:36 -0300

    add boton para visualizar pdf
    en una pestaña distinta

---
+++ resources/views/solicitud_pdf_lodo.blade.php
@@ -0,0 +1,8 @@
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<html>
+    <head>
+        <meta charset="gb18030">
+        <title>Calfrac - Laboratorio de Lodo</title>
+    </head>
+    
+</html>
